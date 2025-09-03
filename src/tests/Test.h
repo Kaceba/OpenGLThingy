6df7efc5cbdbd50f99cf651ae7e2714afb055688
Roberto Vincenzo Ninno <RobertoNinno23@outlook.it>
2025-09-03 10:21:15 +0200

    updated dependencies to be relative paths

--- src/tests/Test.h
+++ src/tests/Test.h
@@ -4,11 +4,12 @@ namespace test {
 
 	class Test
 	{
-		public Test() {}
-		virtual ~Test() {}
+		public:
+			Test() {}
+			virtual ~Test() {}
 
-		virtual void OnUpdate(float deltaTime) {}
-		virtual void OnRender() {}
-		virtual void OnImGuiRender() {}
+			virtual void OnUpdate(float deltaTime) {}
+			virtual void OnRender() {}
+			virtual void OnImGuiRender() {}
 	};
 }
